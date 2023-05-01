6f092cb39a13cb4a7ccec04c1ecfce6e3ccc54c8
Mohamad Qayyum Bin Mohd Nazir <79587343+qayyumnazir@users.noreply.github.com>
2023-05-01 15:51:13 +0000

    added data

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
 #MUST Initilization
 from flask import Flask, render_template
 app = Flask(__name__)
+
+Projects = [
+    {
+        'id': 1,
+        'title': "Cartoon Character Recognition via Object Detection",
+        'software': ['python','streamlit','YOLOv5'],
+        'Description': "A  framework for character presence calculation using YOLOV5 Model with web deployment using streamlit",
+    }
+
+    {
+        'id': 2,
+        'title': "Traffic Light Fuzzy Logic",
+        'software': ['python','sklearn'],
+        'Description': " Traffic light simulation built on the mamdani fuzzy logic system using skfuzzy",
+    }
+
+    {
+        'id': 3,
+        'title': "Diagnosify",
+        'software': ['python','Keras'],
+        'Description': "The building of song/playlist depression indicator using deep learning model",
+    }
+
+    {
+        'id': 4,
+        'title': "ZeroMQ ScreenCapture",
+        'software': ['C','C++','Python','OpenCV'],
+        'Description': "",
+    }
+]
+
 @app.route("/")
 def hello_word():
   return render_template("home.html")
